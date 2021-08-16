6e2fe99ecca5b9ad3f8ac7f3201fea5e5893c94b
MeikeSteinhilber <Meike.Steinhilber@aol.com>
2021-08-16 14:52:35 +0200

    addd: read (csv, txt, xlsx) add: Select seperator string add: different input data (x~y) add: structure for tabs

--- R/load_data.R
+++ R/load_data.R
@@ -1,8 +1,26 @@
+load_data_frame <- function(input) {
+  file_extension <- file_ext(input$upload$datapath)
+  
+  if (file_extension == "csv") {
+    data <- read.csv(input$upload$datapath, sep = input$seperator)
+  # } else if (file_extension == "rda") {
+  #   data <- load(input$upload$datapath)
+  } else if (file_extension == "xlsx") {
+    library(readxl)
+    data <- read_excel(input$upload$datapath)
+  }else if (file_extension == "txt") {
+    data <- read.csv(input$upload$datapath, sep = input$seperator)
+  }
+  data
+}
+
+
 load_data <- function(input) {
   if (is.null(input$upload) && input$load_example_data == FALSE) {
     return(NULL)
   } else if (!is.null(input$upload) && input$load_example_data == FALSE) {
-    df <- read.csv(input$upload$datapath)
+    df <- load_data_frame(input)
+
     if (input$N_reduction == TRUE) {
       df[1:input$N, ]
     } else{
@@ -10,15 +28,12 @@ load_data <- function(input) {
     }
   } else if (input$load_example_data == TRUE) {
     if (input$example_data == "df_stress") {
-      # df <- read.csv("data/df_stress.csv")
       df <- sprtt::df_stress
       df
     } else if (input$example_data == "df_cancer") {
-      # df <- read.csv("data/df_cancer.csv")
       df <- sprtt::df_cancer
       df
     } else if (input$example_data == "df_income") {
-      # df <- read.csv("data/df_income.csv")
       df <- sprtt::df_income
       df
     }
