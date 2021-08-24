2128e7b4a68ee7d3ec9f86bc19bc6a62b94d2e65
Meike <Meike.Steinhilber@aol.com>
2021-08-24 14:19:31 +0200

    error handling: add ID without data

--- R/preprocess_data.R
+++ R/preprocess_data.R
@@ -1,5 +1,11 @@
 preprocess_data <- function(input) {
   data <- load_data(input)
+  
+  # error handling: add ID without data -> ignore input
+  if (is.null(data) && input$id == TRUE) {
+    return(NULL)
+  }
+  
   data <- data_reduction(input, data)
   data <- add_id(input, data)
   data
