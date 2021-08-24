8c78c9b359334d9edbb7687db4c05ae9b59e2b2f
Meike <Meike.Steinhilber@aol.com>
2021-08-24 14:24:28 +0200

    change error message: no data selected

--- app.R
+++ app.R
@@ -188,33 +188,42 @@ server <- function(session, input, output) {
     
     output$seq_ttest_results <- renderPrint({
         data <- data()
-        if (input$long_data == TRUE) {
-            x <- get_x_data(input, data)
-            y <- get_y_data(input, data)
-            
-            sprtt::seq_ttest(x ~ y
-                             , data = data()
-                             , mu = input$mu
-                             , d = input$d
-                             , paired = input$paired
-                             , alternative = input$alternative
-                             , na.rm = input$na.rm
-                             , verbose = input$verbose
-            )
-        } else if ((input$long_data == FALSE)) {
-            x <- get_x_data(input, data)
-            y <- get_y_data(input, data)
-
-            sprtt::seq_ttest(x = x
-                             , y = y
-                             , mu = input$mu
-                             , d = input$d
-                             , paired = input$paired
-                             , alternative = input$alternative
-                             , na.rm = input$na.rm
-                             , verbose = input$verbose
-            )
+        if (is.null(data)) {
+            cat("No data available. Upload you own data or select an examplary data set.")
+        } else {
+            if (input$long_data == TRUE) {
+                x <- get_x_data(input, data)
+                y <- get_y_data(input, data)
+                
+                sprtt::seq_ttest(x ~ y
+                                 , data = data()
+                                 , mu = input$mu
+                                 , d = input$d
+                                 , paired = input$paired
+                                 , alternative = input$alternative
+                                 , alpha = input$alpha
+                                 , power = input$power
+                                 , na.rm = input$na.rm
+                                 , verbose = input$verbose
+                )
+            } else if ((input$long_data == FALSE)) {
+                x <- get_x_data(input, data)
+                y <- get_y_data(input, data)
+                
+                sprtt::seq_ttest(x = x
+                                 , y = y
+                                 , mu = input$mu
+                                 , d = input$d
+                                 , paired = input$paired
+                                 , alternative = input$alternative
+                                 , alpha = input$alpha
+                                 , power = input$power
+                                 , na.rm = input$na.rm
+                                 , verbose = input$verbose
+                )
+            }
         }
+        
     })
 }
 
