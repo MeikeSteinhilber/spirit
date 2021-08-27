e20dcdcdc34a8565a86574ad72551642cd647466
Meike <Meike.Steinhilber@aol.com>
2021-08-27 16:25:45 +0200

    experiment: small test app

--- app.R
+++ app.R
@@ -1,4 +1,12 @@
 # This file is necessary for the publication on shinyapps.io
 # However, it is ignored for building the package
-pkgload::load_all(".")
-myApp()
+# pkgload::load_all(".")
+# myApp()
+
+library(shiny)
+ui <- fluidPage(
+  "Hello, world!"
+)
+server <- function(input, output, session) {
+}
+shinyApp(ui, server)
