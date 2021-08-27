50fd640d219c2014d5044d51c5d18879ffe26551
Meike <Meike.Steinhilber@aol.com>
2021-08-27 10:14:59 +0200

    add new tests

--- tests/testthat/test-load_data.R
+++ tests/testthat/test-load_data.R
@@ -1,3 +1,4 @@
+# no data ----------------------------------------------------------------------
 test_that("load_data: no data", {
   input <- list(
     upload = NULL,
@@ -11,7 +12,8 @@ test_that("load_data: no data", {
   )
 })
 
-test_that("load_data: upload data", {
+# upload csv data --------------------------------------------------------------
+test_that("load_data: upload data csv", {
   # create temporary data set
   path <- tempfile(fileext = ".csv")
   n <- 20
@@ -34,6 +36,7 @@ test_that("load_data: upload data", {
   )
 })
 
+# examplary data selected ------------------------------------------------------
 test_that("load_data: examplary data", {
   input <- list(
     upload = NULL,
@@ -46,8 +49,18 @@ test_that("load_data: examplary data", {
     load_data(input),
     sprtt::df_stress
   )
+  input$example_data <- "df_cancer"
+  expect_equal(
+    load_data(input),
+    sprtt::df_cancer
+  )
+  input$example_data <- "df_income"
+  expect_equal(
+    load_data(input),
+    sprtt::df_income
+  )
 })
-
+# multiple data sets -----------------------------------------------------------
 test_that("load_data: upload data & select examle data", {
   # create temporary data set
   path <- tempfile(fileext = ".csv")
@@ -69,5 +82,53 @@ test_that("load_data: upload data & select examle data", {
   expect_equal(
     load_data(input),
     sprtt::df_stress
+  )
+})
+
+# other data file formats ------------------------------------------------------
+# THIS TESTS MAY BE DELETED -> same test on a lower level: load_data_frame
+test_that("load_data: upload data txt", {
+  # create temporary data set
+  path <- tempfile(fileext = ".txt")
+  n <- 20
+  df <- data.frame(
+    control = rnorm(n),
+    treatment = rnorm(n, mean = 0.8)
+  )
+  utils::write.table(df, path, sep = ",", row.names = FALSE)
+  # create user input
+  input <- list(
+    upload = list(datapath = path),
+    seperator = ",",
+    header = TRUE,
+    load_example_data = FALSE
+  )
+  # test
+  expect_equal(
+    load_data(input),
+    df
+  )
+})
+
+test_that("load_data: upload data xlsx", {
+  # create temporary data set
+  path <- tempfile(fileext = ".xlsx")
+  n <- 20
+  df <- data.frame(
+    control = rnorm(n),
+    treatment = rnorm(n, mean = 0.8)
+  )
+  writexl::write_xlsx(df, path)
+  # create user input
+  input <- list(
+    upload = list(datapath = path),
+    seperator = ",",
+    header = TRUE,
+    load_example_data = FALSE
+  )
+  # test
+  expect_equal(
+    load_data(input),
+    df
   )
 })
