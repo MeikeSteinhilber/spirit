b6c09369e16ae1eee475516f2e2a53bb3dca46f9
Meike <Meike.Steinhilber@aol.com>
2021-08-27 14:18:24 +0200

    change UI into a function

--- R/ui.R
+++ R/ui.R
@@ -1,10 +1,12 @@
-ui <- fluidPage(
-  h1("Spirit"),
-  strong("App to Calculate Sequential t-Tests"),
-  uiOutput("description_text"),
-  br(),
-  tabsetPanel( 
-    tab_1,
-    tab_2
+ui <- function(request) {
+  fluidPage(
+    h1("Spirit"),
+    strong("App to Calculate Sequential t-Tests"),
+    uiOutput("description_text"),
+    br(),
+    tabsetPanel( 
+      tab_1,
+      tab_2
+    )
   )
-)
+}
