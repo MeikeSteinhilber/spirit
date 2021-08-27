131bbdf2dd9946e4aa71c6a6043f31ee68d538f2
Meike <Meike.Steinhilber@aol.com>
2021-08-27 16:37:41 +0200

    change back to original app

--- R/ui.R
+++ R/ui.R
@@ -1,16 +1,12 @@
-# ui <- function(request) {
-#   fluidPage(
-#     h1("Spirit"),
-#     strong("App to Calculate Sequential t-Tests"),
-#     uiOutput("description_text"),
-#     br(),
-#     tabsetPanel( 
-#       tab_1,
-#       tab_2
-#     )
-#   )
-# }
-
-ui <- fluidPage(
-  "Hello, world!"
-)
+ui <- function(request) {
+  fluidPage(
+    h1("Spirit"),
+    strong("App to Calculate Sequential t-Tests"),
+    uiOutput("description_text"),
+    br(),
+    tabsetPanel(
+      tab_1,
+      tab_2
+    )
+  )
+}
--- app.R
+++ app.R
@@ -1,12 +1,4 @@
 # This file is necessary for the publication on shinyapps.io
 # However, it is ignored for building the package
-# pkgload::load_all(".")
-# myApp()
-
-library(shiny)
-# ui <- fluidPage(
-#   "Hello, world!"
-# )
-server <- function(input, output, session) {
-}
-shinyApp(ui, server)
+pkgload::load_all(".")
+myApp()
