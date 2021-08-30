99d151bc080e4ebc90ccec6539cc2f46ec700dec
Meike <Meike.Steinhilber@aol.com>
2021-08-30 09:36:31 +0200

    options(shiny.autoload.r=FALSE)

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 # This file is necessary for the publication on shinyapps.io
 # However, it is ignored for building the package
+options(shiny.autoload.r=FALSE)
 pkgload::load_all(".")
-myApp()
+spirit::myApp()
