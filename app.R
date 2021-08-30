7f7bbb6ed520074c8e36f0a1795d014016f5edb2
Meike <Meike.Steinhilber@aol.com>
2021-08-30 09:43:01 +0200

    deactivate autoload

--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 # This file is necessary for the publication on shinyapps.io
 # However, it is ignored for building the package
-options(shiny.autoload.r=FALSE)
+# options(shiny.autoload.r=FALSE)
 pkgload::load_all(".")
 spirit::myApp()
