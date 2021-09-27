525b5038b95b8ede51fd34d42caeaa4d2be9e1ac
Meike <Meike.Steinhilber@aol.com>
2021-09-27 11:50:15 +0200

    fix error: :: shinytest

--- tests/testthat/test-myApp.R
+++ tests/testthat/test-myApp.R
@@ -1,5 +1,5 @@
 test_that("myApp: snapshot", {
-  skip_on_cran() # skip on CRAN checks: necessary!
+  skip_on_cran() # skip on CRAN checks
   skip_on_ci() # skip on continuous platforms like GitHub Actions
   skip("weird error message") # skip test
   # 3.ed edition necessary for expect_snapshot
@@ -11,13 +11,16 @@ test_that("myApp: snapshot", {
 
   # Save screenshot to temporary file
   # Tab 1 ---
-  shinytest::app$takeScreenshot(path)
-  shinytest::expect_snapshot_file(path, "tab1_plot.png")
+  app$takeScreenshot(path)
+  testthat::expect_snapshot_file(path, "tab1_plot.png")
   # Tab 2 ---
-  shinytest::app$setInputs(tabs_id = "Test Specification")
-  shinytest::app$setInputs(mu = 2)
-  shinytest::app$setInputs(na_rm = FALSE)
-  shinytest::app$takeScreenshot(path)
-  shinytest::expect_snapshot_file(path, "tab2_plot.png")
+  # app$setInputs(tabs_id = "Test Specification")
+  # app$setInputs(mu = 2)
+  # app$setInputs(na_rm = FALSE)
+  # app$takeScreenshot(path)
+  # testthat::expect_snapshot_file(path,
+  #                                "tab2_plot.png",
+  #                                compare = "compare_file_text"
+  #                                )
   # app$listWidgets()
 })
